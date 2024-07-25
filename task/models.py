f4fc2d796944ceae7d79c8f07ed74183ac066de2
Vlad <vlad@example.com>
2024-07-25 15:40:41 +0300

    added file models.py with 4 models Position, Worker(AbstractUser), TaskType, Task

--- task/models.py
+++ task/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    deadline = models.DateField()
+    is_completed = models.BooleanField()
+    priority = models.CharField(
+        max_length=1,
+        choices=[
+            ("U", "Urgent"),
+            ("H", "High"),
+            ('M', 'Medium'),
+            ('L', 'Low'),
+        ],
+        default="M",
+    )
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE,
+        related_name="tasks",
+    )
+    assignees = models.ManyToManyField(
+        Worker,
+        related_name="tasks"
+    )
